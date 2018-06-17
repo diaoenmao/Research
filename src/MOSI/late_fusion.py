80834f39b0f8aeff327837fc34c37837ed1e7cc9
dem123456789 <diao_em@hotmail.com>
2018-06-16 21:20:21 -0400

    late fusion done

--- src/MOSI/late_fusion.py
+++ src/MOSI/late_fusion.py
@@ -13,7 +13,7 @@ from modelWrapper import *
 
 data_name = 'MOSI'
 model_dir = 'mosi'
-model_name = 'lstm'
+model_name = 'conv'
 TAG = data_name+'_'+model_name
 config.init()
 batch_size = config.PARAM['batch_size']
@@ -23,9 +23,10 @@ verbose = config.PARAM['verbose']
 if_resume = config.PARAM['if_resume']
 if_show = config.PARAM['if_show']
 num_Experiments = 1
-input_feature = 420
+input_feature = [46,74,300]
 output_feature = 7
 cudnn.benchmark = True
+modality_name = ['visual','audio','text']
 
 def main():
     if(if_resume):
@@ -35,11 +36,11 @@ def main():
         init_seed = 0
     seeds = list(range(init_seed,init_seed+num_Experiments))
     for i in range(len(seeds)):
-        runExperiment(seeds[i],'{}_{}'.format(TAG,seeds[i]))
+        runExperiment('{}_{}'.format(TAG,seeds[i]))
     return
     
-def runExperiment(seed,Experiment_TAG):
-    TAG = Experiment_TAG
+def runExperiment(TAG):
+    seed = TAG.split('_')[-1]
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     visual_train_loader,visual_valid_loader,visual_eval_loader,visual_test_loader = fetch_Multimodal_data(data_name,'visual',batch_size)
@@ -56,79 +57,39 @@ def runExperiment(seed,Experiment_TAG):
     valid_loader = [visual_valid_loader,audio_valid_loader,text_valid_loader]
     eval_loader = [visual_eval_loader,audio_eval_loader,text_eval_loader]
     test_loader = [visual_test_loader,audio_test_loader,text_test_loader]
-    modality_name = ['visual','audio','text']
-    modality_valid_acc = np.zeros(3)
+    model_weight = np.zeros(3)
+    print("Compute model weight")
+    mw = create_mw()
     for i in range(len(modality_name)):
-        model_weight[i] = train_modality(train_loader[i],valid_loader[i],TAG+modality_name[i])
+        print('Train '+ modality_name[i])
+        model_weight[i] = train_modality(train_loader[i],valid_loader[i],mw[i],TAG+'_'+modality_name[i])
     model_weight = model_weight/np.sum(model_weight)
+    print(model_weight)
+    print("Retrain Model")
+    mw = create_mw()
     for i in range(len(modality_name)):
-        train_modality(eval_loader[i],test_loader[i],TAG+modality_name[i])
-    model_average(test_loader,model_weight,TAG+modality_name[i])
-    if(if_show):
-        plt_result(TAG+modality_name[i])
+        print('Train '+ modality_name[i])
+        train_modality(eval_loader[i],test_loader[i],mw[i],TAG+'_'+modality_name[i])
+    print("Evaluate Model")
+    mw = create_mw()
+    test_modality(test_loader,model_weight,mw,TAG)
     return
 
-def model_average(test_loader,model_weight,modality_name,TAG):
-    criterion = nn.CrossEntropyLoss().to(device)
-    model = eval('models.{}.{}(input_feature={},output_feature={}).to(device)'.format(model_dir,model_name,input_feature,output_feature))
-    mw = modelWrapper(model,config.PARAM['optimizer_name'])
-    mw.set_optimizer_param(config.PARAM['optimizer_param'])
-    mw.set_criterion(criterion)
-    mw.wrap()
+def create_mw():
+    mw = []
+    for i in range(len(modality_name)): 
+        criterion = nn.CrossEntropyLoss().to(device)
+        model = eval('models.{}.{}(input_feature={},output_feature={}).to(device)'.format(model_dir,model_name,input_feature[i],output_feature))
+        mw.append(modelWrapper(model,config.PARAM['optimizer_name']))
+        mw[i].set_optimizer_param(config.PARAM['optimizer_param'])
+        mw[i].set_criterion(criterion)
+        mw[i].wrap()
+    return mw
     
-    batch_time = Meter()
-    data_time = Meter()
-    losses = Meter()
-    top1 = Meter()
-    top5 = Meter()
-    mw.model.eval()
-    with torch.no_grad():
-        end = time.time()
-        for i, (input, target) in enumerate(val_loader):
-            input, target = input.to(device), target.to(device)
-            data_time.update(time.time() - end)
-            best = torch.load('./output/model/best_{}.pth'.format(TAG))
-            mw.model.load_state_dict(best['state_dict'])
-            if(model_name=='lstm'):
-                mw.model.hidden = mw.model.init_hidden(input.size(0))
-            output = mw.model(input)
-            loss = mw.loss(output,target)
-            
-            
-            
-            losses.update(loss.item(), input.size(0))
-            if(output_feature>5):
-                prec1, prec5 = mw.acc(output, target, topk=(1, 5))
-            else:
-                prec1, prec5 = mw.acc(output, target, topk=(1, 1))
-            top1.update(prec1[0], input.size(0))
-            top5.update(prec5[0], input.size(0))
-            batch_time.update(time.time() - end)
-            end = time.time()
-
-            if(verbose):
-                print('Test: [{0}/{1}]\t'
-                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                       i+1, len(val_loader), batch_time=batch_time,
-                       data_time=data_time, loss=losses, top1=top1, top5=top5))
-              
-    return batch_time,data_time,losses,top1,top5    
-
-                   
-def train_modality(train_loader,test_loader,TAG):
-    criterion = nn.CrossEntropyLoss().to(device)
-    model = eval('models.{}.{}(input_feature={},output_feature={}).to(device)'.format(model_dir,model_name,input_feature,output_feature))
-    mw = modelWrapper(model,config.PARAM['optimizer_name'])
-    mw.set_optimizer_param(config.PARAM['optimizer_param'])
-    mw.set_criterion(criterion)
-    mw.wrap()
- 
+def train_modality(train_loader,test_loader,mw,TAG):
+    seed = TAG.split('_')[-2]
     if(if_resume):
-        checkpoint = torch.load('./output/model/checkpoint.pth')
+        checkpoint = torch.load('./output/model/checkpoint_{}.pth'.format(TAG))
         init_epoch = checkpoint['epoch']
         best_prec1 = checkpoint['best_prec1']
         best_epoch = checkpoint['best_epoch']
@@ -147,7 +108,7 @@ def train_modality(train_loader,test_loader,TAG):
 
     for epoch in range(init_epoch,init_epoch+max_num_epochs):
         scheduler.step()
-        new_train_result = train(epoch,eval_loader,mw)
+        new_train_result = train(epoch,train_loader,mw)
         new_test_result = test(test_loader, mw)
         prec1 = new_test_result[3].avg
         is_best = prec1 > best_prec1
@@ -172,14 +133,63 @@ def train_modality(train_loader,test_loader,TAG):
             'optimizer' : mw.optimizer.state_dict(),
         }, is_best,TAG)
         
-    best = load('./output/model/best_{}.pth'.format(seed))
+    best = load('./output/model/best_{}.pth'.format(TAG))
     mw.model.load_state_dict(best['state_dict'])
     mw.optimizer.load_state_dict(best['optimizer'])    
     final_test_result = test(test_loader, mw)
-    save(final_test_result,'./output/result/final_{}'.format(Experiment_TAG))
+    save(final_test_result,'./output/result/final_{}'.format(TAG))
     print('Test Result:')
     print_meter(final_test_result)
-    return final_test_result[3]
+    return final_test_result[3].avg
+   
+def test_modality(test_loader,model_weight,mw,TAG):   
+    batch_time = Meter()
+    data_time = Meter()
+    losses = Meter()
+    top1 = Meter()
+    top5 = Meter()
+    list_test_loader = []
+    best = []
+    for i in range(len(modality_name)):
+        mw[i].model.eval()
+        list_test_loader.append(list(test_loader[i]))
+        best.append(torch.load('./output/model/best_{}.pth'.format(TAG+'_'+modality_name[i])))
+    end = time.time()
+    with torch.no_grad():
+        num_batch = len(list_test_loader[0])
+        for i in range(num_batch):
+            output = 0
+            for j in range(len(modality_name)):
+                input, target = list_test_loader[j][i][0],list_test_loader[j][i][1]
+                input, target = input.to(device), target.to(device)
+                data_time.update(time.time() - end)              
+                mw[j].model.load_state_dict(best[j]['state_dict'])
+                if(model_name=='lstm'):
+                    mw[j].model.hidden = mw[j].model.init_hidden(input.size(0))
+                output += model_weight[j].item()*mw[j].model(input)
+                batch_time.update(time.time() - end)
+                end = time.time()                
+            loss = mw[0].loss(output,target)           
+            losses.update(loss.item(), input.size(0))
+            if(output_feature>5):
+                prec1, prec5 = mw[0].acc(output, target, topk=(1, 5))
+            else:
+                prec1, prec5 = mw[0].acc(output, target, topk=(1, 1))
+            top1.update(prec1[0], input.size(0))
+            top5.update(prec5[0], input.size(0))
+            if(verbose):
+                print('Test: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                       i+1, len(num_batch), batch_time=batch_time,
+                       data_time=data_time, loss=losses, top1=top1, top5=top5))
+    test_result = [batch_time,data_time,losses,top1,top5]     
+    print('Test Result:')
+    print_meter(test_result)
+    return test_result[3].avg
     
 def train(epoch,train_loader, mw):
     batch_time = Meter()
