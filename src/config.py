14be493dee4c5dd8550fb1faa7230d78b2ed529d
dem123456789 <diao_em@hotmail.com>
2018-06-20 14:36:52 -0400

    update

--- src/config.py
+++ src/config.py
@@ -3,9 +3,9 @@ import torch
 def init():
     global PARAM
     PARAM = {
-        'optimizer_param': {'lr': 1e-2, 'momentum': 0.9, 'weight_decay': 5e-4},
+        'optimizer_param': {'lr': 1e-1, 'momentum': 0.9, 'weight_decay': 5e-4},
         'optimizer_name': 'SGD',
-        'batch_size': 100,
+        'batch_size': 30,
         'data_size': 50000,
         'device': 'cuda:0',
         'if_resume': False,
