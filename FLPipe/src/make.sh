30fb406fc9a71b1fdc4df259d98f420bb1710c21
DreamSoul <diao_em@hotmail.com>
2024-02-07 21:03:33 -0500

    update

--- FLPipe/src/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python make.py --mode base --run train --num_experiment 4 --round 8
-python make.py --mode base --run test --num_experiment 4 --round 8
-
-python make.py --mode fl --run train --num_experiment 4 --round 16 --split_round 2
-python make.py --mode fl --run test --num_experiment 4 --round 16 --split_round 2
--- PEFTPipe/src/dataset/__init__.py
+++
@@ -1,6 +0,0 @@
-from .dataset import *
-from .utils import *
-from .mnist import MNIST, FashionMNIST
-from .cifar import CIFAR10, CIFAR100
-from .svhn import SVHN
-from .dreambooth import DreamBooth
